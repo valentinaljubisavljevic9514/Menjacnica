aa35505c28219cc25f2d7565a589e197711b24d8
valentinaljubisavljevic9514 <vanja.vale95@gmail.com>
2016-03-20 17:01:51 +0100

    U klase Valuta i Kurs su dodate metode toString, equals i hashCode.

--- Menjacnica/src/valuta/Valuta.java
+++ Menjacnica/src/valuta/Valuta.java
@@ -28,6 +28,47 @@ public class Valuta {
 	public void setKraciNazivValute(String kraciNazivValute) {
 		this.kraciNazivValute = kraciNazivValute;
 	}
+	@Override
+	public String toString() {
+		return "Valuta [nazivValute=" + nazivValute + ", kraciNazivValute=" + kraciNazivValute + ", kursValute="
+				+ kursValute + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kraciNazivValute == null) ? 0 : kraciNazivValute.hashCode());
+		result = prime * result + ((kursValute == null) ? 0 : kursValute.hashCode());
+		result = prime * result + ((nazivValute == null) ? 0 : nazivValute.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (kraciNazivValute == null) {
+			if (other.kraciNazivValute != null)
+				return false;
+		} else if (!kraciNazivValute.equals(other.kraciNazivValute))
+			return false;
+		if (kursValute == null) {
+			if (other.kursValute != null)
+				return false;
+		} else if (!kursValute.equals(other.kursValute))
+			return false;
+		if (nazivValute == null) {
+			if (other.nazivValute != null)
+				return false;
+		} else if (!nazivValute.equals(other.nazivValute))
+			return false;
+		return true;
+	}
+	
 	
 	
 }
