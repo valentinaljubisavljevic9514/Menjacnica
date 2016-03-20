896331c5bf58aa816e02307bf77920f862034d32
valentinaljubisavljevic9514 <vanja.vale95@gmail.com>
2016-03-20 16:56:43 +0100

    Napravljene su klase Valute i Kurs sa svojim atributima i njihovim get i set metodama.

---
+++ Menjacnica/src/kursevi/Kurs.java
@@ -0,0 +1,38 @@
+package kursevi;
+
+import java.util.GregorianCalendar;
+
+public class Kurs {
+
+	private GregorianCalendar datum = new GregorianCalendar();
+	private double prodajniKurs;
+	private double kupovniKurs;
+	private double srednjiKurs;
+	
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+	public double getProdajniKurs() {
+		return prodajniKurs;
+	}
+	public void setProdajniKurs(double prodajniKurs) {
+		this.prodajniKurs = prodajniKurs;
+	}
+	public double getKupovniKurs() {
+		return kupovniKurs;
+	}
+	public void setKupovniKurs(double kupovniKurs) {
+		this.kupovniKurs = kupovniKurs;
+	}
+	public double getSrednjiKurs() {
+		return srednjiKurs;
+	}
+	public void setSrednjiKurs(double srednjiKurs) {
+		this.srednjiKurs = srednjiKurs;
+	}
+	
+	
+}
---
+++ Menjacnica/src/valuta/Valuta.java
@@ -0,0 +1,22 @@
+package valuta;
+
+public class Valuta {
+
+	private String nazivValute;
+	private String kraciNazivValute;
+	
+	public String getNazivValute() {
+		return nazivValute;
+	}
+	public void setNazivValute(String nazivValute) {
+		this.nazivValute = nazivValute;
+	}
+	public String getKraciNazivValute() {
+		return kraciNazivValute;
+	}
+	public void setKraciNazivValute(String kraciNazivValute) {
+		this.kraciNazivValute = kraciNazivValute;
+	}
+	
+	
+}
