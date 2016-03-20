fe70da5c826099fee6cef93b43ee58e057b32724
valentinaljubisavljevic9514 <vanja.vale95@gmail.com>
2016-03-20 17:31:33 +0100

    Implementirana je do kraja klasa Menjacnica na grani imlepementacijainterfejsa.

--- Menjacnica/src/impementacija/Menjacnica.java
+++ Menjacnica/src/impementacija/Menjacnica.java
@@ -2,25 +2,38 @@ package impementacija;
 
 import java.util.GregorianCalendar;
 
+
 import interfejs.InterfejsMenjacnice;
 import kursevi.Kurs;
 import valuta.Valuta;
 
 public class Menjacnica implements InterfejsMenjacnice {
 
+	
 	@Override
 	public void dodajKurs(Valuta valuta, Kurs kurs) {
 		
-
+		valuta.getKursValute().add(kurs);
+		
 	}
 
 	@Override
 	public void obrisiKurs(Valuta valuta, Kurs kurs) {
 
+		valuta.getKursValute().remove(kurs);
+		
 	}
 
 	@Override
 	public Kurs vratiKurs(GregorianCalendar datum, Valuta valuta) {
+		
+		for (int i = 0; i < valuta.getKursValute().size(); i++) {
+			if(valuta.getKursValute().get(i).getDatum() == datum &&
+					valuta.getNazivValute().equals(valuta.getNazivValute())){
+				return valuta.getKursValute().get(i);
+			}
+			
+		}
 		
 		return null;
 	}
