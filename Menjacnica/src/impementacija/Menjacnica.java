db5f9dc5480f504b9eb6d75c12e847d7b0b32094
valentinaljubisavljevic9514 <vanja.vale95@gmail.com>
2016-03-20 18:11:44 +0100

    Implementovane su metode klase Menjacnica.

--- Menjacnica/src/impementacija/Menjacnica.java
+++ Menjacnica/src/impementacija/Menjacnica.java
@@ -8,21 +8,39 @@ import valuta.Valuta;
 
 public class Menjacnica implements InterfejsMenjacnice {
 
-	@Override
-	public void dodajKurs(Valuta valuta, Kurs kurs) {
-		
 
-	}
 
-	@Override
-	public void obrisiKurs(Valuta valuta, Kurs kurs) {
+		@Override
+		public void dodajKurs(Valuta valuta, Kurs kurs) {
+			
+			if(kurs == null) throw new NullPointerException();
+			
+			valuta.getKursValute().addLast(kurs);
 
-	}
+		}
 
-	@Override
-	public Kurs vratiKurs(GregorianCalendar datum, Valuta valuta) {
-		
-		return null;
-	}
+		@Override
+		public void obrisiKurs(Valuta valuta, Kurs kurs) {
+			if(kurs == null) throw new NullPointerException();
+			
+			valuta.getKursValute().remove(kurs);
+			
+		}
+
+		@Override
+		public Kurs vratiKurs(GregorianCalendar datum, Valuta valuta) {
+			
+			if(datum == null) throw new NullPointerException();
+			
+			for (int i = 0; i < valuta.getKursValute().size(); i++) {
+				if(valuta.getKursValute().get(i).getDatum() == datum){
+					return valuta.getKursValute().get(i);
+				}
+				
+			}
+			
+			return null;
+		}
 
 }
+
